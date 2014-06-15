713dfd472a01f483f3a057dc3a0bb56b8d4ddba2
Alexey Fedorov <waterlink000@gmail.com>
2014-06-16 00:30:54 +0300

    Initial commit

---
+++ lib/deploy/code.rb
@@ -0,0 +1,7 @@
+require "deploy/code/version"
+
+module Deploy
+  module Code
+    # Your code goes here...
+  end
+end
---
+++ lib/deploy/code/version.rb
@@ -0,0 +1,5 @@
+module Deploy
+  module Code
+    VERSION = "0.0.1"
+  end
+end
