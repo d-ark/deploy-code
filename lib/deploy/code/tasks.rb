4005b2ad14268b0db16928b5adfa2ca16f67d83b
Anton Priadko <antonpriadko+scs@gmail.com>
2014-07-01 16:34:43 +0300

    [removed verbose]

--- lib/deploy/code/tasks.rb
+++ lib/deploy/code/tasks.rb
@@ -33,7 +33,7 @@ Deploy::Code.within_capistrano do
   set :git_shallow_clone, 1
 
   set :scm_verbose, 10
-  set :verbose, 10
+  # set :verbose, 10
 
   ENV['SERVERS'].split(',').each do |server|
     role :app, server
